e66fcfecdab50a4c20094ce3ec319263f25ded3b
Josh Driver <keeperofdakeys@gmail.com>
2016-03-25 05:16:31 +0950

    Update documentation

--- implementors/core/marker/trait.Send.js
+++ implementors/core/marker/trait.Send.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['dns_lookup'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='dns_lookup/lookup/struct.LookupHost.html' title='dns_lookup::lookup::LookupHost'>LookupHost</a>",];implementors['dns_lookup'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='dns_lookup/lookup/struct.LookupHost.html' title='dns_lookup::lookup::LookupHost'>LookupHost</a>",];
+implementors['libc'] = [];implementors['dns_lookup'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='dns_lookup/lookup/struct.LookupHost.html' title='dns_lookup::lookup::LookupHost'>LookupHost</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/marker/trait.Sync.js
+++ implementors/core/marker/trait.Sync.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['dns_lookup'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html' title='core::marker::Sync'>Sync</a> for <a class='struct' href='dns_lookup/lookup/struct.LookupHost.html' title='dns_lookup::lookup::LookupHost'>LookupHost</a>",];implementors['dns_lookup'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html' title='core::marker::Sync'>Sync</a> for <a class='struct' href='dns_lookup/lookup/struct.LookupHost.html' title='dns_lookup::lookup::LookupHost'>LookupHost</a>",];
+implementors['libc'] = [];implementors['dns_lookup'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html' title='core::marker::Sync'>Sync</a> for <a class='struct' href='dns_lookup/lookup/struct.LookupHost.html' title='dns_lookup::lookup::LookupHost'>LookupHost</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
