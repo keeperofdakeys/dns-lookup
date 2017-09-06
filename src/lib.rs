c5f2117885268a578deaaa7216a3397640574acb
Josh Driver <keeperofdakeys@gmail.com>
2017-09-06 22:53:14 +0930

    Export more symbols

--- src/lib.rs
+++ src/lib.rs
@@ -38,4 +38,4 @@ mod err;
 mod lookup;
 
 pub use lookup::{lookup_host, lookup_addr};
-pub use addrinfo::{getaddrinfo, AddrInfoIter};
+pub use addrinfo::{getaddrinfo, AddrInfoIter, AddrInfo, AddrInfoHints};
