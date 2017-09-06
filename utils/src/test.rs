5af57f2b0c760f91fbcbe6e2d741d475254d72eb
Josh Driver <keeperofdakeys@gmail.com>
2017-09-07 00:15:59 +0930

    Small test for unicode lookup

--- utils/src/test.rs
+++ utils/src/test.rs
@@ -5,7 +5,14 @@ use std::io;
 
 fn main() {
   let mut hints = dns::AddrInfoHints::default();
-  // hints.flags = 0x0040;
+  hints.flags = 0x0040;
+  // hints.socktype = dns::SockType::Stream;
+  // hints.address = dns::AddrFamily::Inet;
+  // hints.protocol = dns::ProtoFamily::Inet;
+  unsafe {
+    let cstr = std::ffi::CString::new("").unwrap();
+    libc::setlocale(libc::LC_ALL, cstr.as_ptr() as *const _ as *const i8);
+  }
   let list: io::Result<Vec<_>> =
     dns::getaddrinfo(Some("☃.net"), Some("http"), Some(hints)).unwrap().collect();
   println!("{:?}", list);
