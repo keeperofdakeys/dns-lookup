9d99be3817a8ef81a62110cc96b4175133b11bc8
Josh Driver <keeperofdakeys@gmail.com>
2017-10-13 08:09:35 +0950

    Update documentation

--- implementors/std/io/trait.Read.js
+++ implementors/std/io/trait.Read.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["libc"] = [];
+implementors["socket2"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"socket2/struct.Socket.html\" title=\"struct socket2::Socket\">Socket</a>","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for &amp;'a <a class=\"struct\" href=\"socket2/struct.Socket.html\" title=\"struct socket2::Socket\">Socket</a>","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for &amp;'a Socket",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/std/sys/imp/ext/io/trait.FromRawFd.js
+++ implementors/std/sys/imp/ext/io/trait.FromRawFd.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["libc"] = [];
+implementors["socket2"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/sys/imp/ext/io/trait.FromRawFd.html\" title=\"trait std::sys::imp::ext::io::FromRawFd\">FromRawFd</a> for <a class=\"struct\" href=\"socket2/struct.Socket.html\" title=\"struct socket2::Socket\">Socket</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
