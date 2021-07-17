581e8cab9611a57dda9068dc8c7083444c6a20ae
Josh Driver <keeperofdakeys@gmail.com>
2021-07-17 18:30:15 +0930

    Bump version

--- utils/src/test.rs
+++ utils/src/test.rs
@@ -2,6 +2,7 @@ extern crate libc;
 extern crate dns_lookup as dns;
 
 use std::io;
+use dns::LookupErrorKind;
 
 fn main() {
   let mut hints = dns::AddrInfoHints::default();
@@ -16,4 +17,14 @@ fn main() {
   let list: io::Result<Vec<_>> =
     dns::getaddrinfo(Some("☃.net"), Some("http"), Some(hints)).unwrap().collect();
   println!("{:?}", list);
+  let foo = dns::getaddrinfo(Some("☃.net"), Some("foobar"), Some(hints));
+  match foo {
+    Ok(_) => {},
+    Err(e) => match e.kind() {
+      LookupErrorKind::NoName => println!("NoName"),
+      _ => println!("{:?}", e),
+    }
+  }
+  let bar = dns::LookupError::new(0);
+  println!("{:?} {:?}", bar, LookupErrorKind::new(0));
 }
